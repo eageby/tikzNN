922ccf6d47ad732be34875b3c499a50c3a15bb91
Elias Ågeby <eageby@kth.se>
2019-11-21 18:49:08 +0100

    First version

---
+++ netgenerator.py
@@ -0,0 +1,66 @@
+import csv
+import itertools
+import os
+
+import click
+import numpy as np
+
+def normalize(layer_widths):
+    return list((layer_widths - np.max(layer_widths)) / 2) 
+def vertices(layer_widths, directory, x_separation=2, y_separation=1.5, **kwargs):
+    normalizing_width = normalize(layer_widths)
+
+    with open("{}/vertices.csv".format(directory), "w", newline="") as file:
+        csv_writer = csv.writer(file, delimiter=",")
+        csv_writer.writerow(
+            ["id", "x", "y", "size", "color", "opacity", "label", "layer"]
+        )
+
+        for layer in range(len(layer_widths)):
+            for node in range(layer_widths[layer]):
+                x_pos = x_separation * layer
+                y_pos = y_separation * (normalizing_width[layer] - node)
+                csv_writer.writerow(["l{}n{}".format(layer, node), x_pos, y_pos])
+
+
+def edges(layer_widths, directory, neuron_connections=None, **kwargs):
+    normalizing_width = normalize(layer_widths)
+
+    with open("{}/edges.csv".format(directory), "w", newline="") as file:
+        csv_writer = csv.writer(file, delimiter=",")
+        csv_writer.writerow(
+            ["u", "v", "label", "lw", "color", "opacity", "bend", "Direct"]
+        )
+
+        for layer in range(len(layer_widths) - 1):
+            first_layer_nodes = [
+                "l{}n{}".format(layer, j) for j in range(layer_widths[layer])
+            ]
+
+            for n, _ in enumerate(first_layer_nodes):
+                connecting_nodes = [
+                    "l{}n{}".format(layer + 1, j)
+
+                    for j in range(layer_widths[layer + 1])
+
+                    if neuron_connections is None or abs(n-j) < neuron_connections
+                ]
+                edges_ = list(
+                    itertools.product([first_layer_nodes[n]], connecting_nodes)
+                )
+
+                for e in edges_:
+                    csv_writer.writerow(e)
+
+
+@click.command()
+@click.argument("layer_widths", nargs=-1, type=int)
+@click.option("--directory", "-d", default="./network", type=click.Path())
+@click.option("--x_separation", "-x", default=2, type=float)
+@click.option("--y_separation", "-y", default=1.5, type=float)
+@click.option("--neuron_connections", "-c", type=int)
+def network(layer_widths, directory, **kwargs):
+    os.makedirs(directory, exist_ok=True)
+
+    vertices(layer_widths, directory, **kwargs)
+    edges(layer_widths, directory, **kwargs)
